107d68315d822bb837587cdb021e0a88336d2b09
hawwokitty <hannesofiea@gmail.com>
2024-05-22 14:28:01 +0200

    bosssssss

---
+++ BossFight/BossFight/GameCharacter.cs
@@ -0,0 +1,35 @@
+﻿namespace BossFight
+{
+    internal class GameCharacter(string type, int health, int strength, int stamina)
+    {
+        public string Type { get; private set; } = type;
+        public int Health { get; private set; } = health;
+        public int Strength { get; private set; } = strength;
+        public int Stamina { get; private set; } = stamina;
+
+        public void Fight(GameCharacter characterBeingAttacked)
+        {
+            if (Type == "Boss")
+            {
+                Random randomNum = new Random();
+                Strength = randomNum.Next(0, 20);
+            }
+            if (Stamina > 0)
+            {
+                characterBeingAttacked.Health = characterBeingAttacked.Health - Strength;
+                Stamina -= 10;
+                Console.WriteLine($"{Type} hit {characterBeingAttacked.Type} for {Strength} damage, {characterBeingAttacked.Type} now has {characterBeingAttacked.Health} left");
+            }
+            else
+            {
+                Console.WriteLine($"{Type} had to recharge this turn");
+                Recharge();
+            }
+        }
+
+        public void Recharge()
+        {
+            Stamina = stamina;
+        }
+    }
+}
---
+++ BossFight/BossFight/Program.cs
@@ -0,0 +1,33 @@
+﻿using BossFight;
+
+GameCharacter hero = new GameCharacter("Hero", 100, 20, 40);
+GameCharacter boss = new GameCharacter("Boss", 400, 20, 10);
+
+RunMatch();
+
+void RunMatch()
+{
+    while (hero.Health > 0 && boss.Health > 0)
+    {
+        hero.Fight(boss);
+        boss.Fight(hero);
+    }
+
+    RunCheckWinner();
+}
+
+void RunCheckWinner()
+{
+    string winner = "";
+    if (hero.Health > 0)
+    {
+        winner = hero.Type;
+    }
+    else
+    {
+        winner = boss.Type;
+
+    }
+
+    Console.WriteLine($"The winner is the {winner}");
+}
