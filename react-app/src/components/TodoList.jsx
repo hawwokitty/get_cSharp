70e7a85907a4641f3db61d9e609145277fa8918b
hawwokitty <hannesofiea@gmail.com>
2024-08-14 14:05:14 +0200

    todo app basic

---
+++ react-app/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import TodoItem from "./TodoItem";
+
+function TodoList(props) {
+  const items = props.tasks;
+  const toggleTask = props.toggleTask;
+  const deleteTask = props.deleteTask;
+
+  return items.map((item, index) => {
+    return <TodoItem key={index} item={item} toggleTask={toggleTask} deleteTask={deleteTask} />;
+  });
+}
+
+export default TodoList;
